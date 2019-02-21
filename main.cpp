927b95b99534476b6b6f9a46f05db4bea501124b
Parth <prth.2000@gmail.com>
2019-02-20 19:06:48 -0500

    PID constants near set. Finished  interaction up until lever.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,21 @@ DigitalEncoder left_encoder(FEHIO::P0_1);
 FEHMotor right_motor(FEHMotor::Motor0,9);
 FEHMotor left_motor(FEHMotor::Motor1,9);
 
+void writeScreen(int leftEncoder, int rightEncoder, int leftSpeed, int rightSpeed, int totalTicks){
+    LCD.Clear();
+    LCD.WriteRC(leftEncoder,4,0);
+    LCD.WriteRC(rightEncoder,5,0);
+    LCD.WriteRC(leftSpeed,6,0);
+    LCD.WriteRC(rightSpeed,7,0);
+    LCD.WriteRC(totalTicks,8,0);
+}
 
 void driveStraightDistance(double tenthsOfIn, int masterPower)
 {
   int tickGoal = ( tenthsOfIn/10) * 40.5;
+  int leftPrior = 0;
+  int rightPrior = 0;
+
 
   //This will count up the total encoder ticks despite the fact that the encoders are constantly reset.
   int totalTicks = 0;
@@ -21,14 +32,14 @@ void driveStraightDistance(double tenthsOfIn, int masterPower)
   //Initialise slavePower as masterPower - 5 so we don't get huge error for the first few iterations. The
   //-5 value is based off a rough guess of how much the motors are different, which prevents the robot from
   //veering off course at the start of the function.
-  int slavePower = masterPower + 5;
+  int slavePower = masterPower - 3;
 
   int error = 0;
   int error_prior =0;
 
-  double kp = 0.15;
-  //double ki;
-  double kd = 30.0 ; //18
+  double kp = 1.91325 ;
+  double ki = .014;
+  double kd = .5 ; //18
 
   left_encoder.ResetCounts();
   right_encoder.ResetCounts();
@@ -36,20 +47,23 @@ void driveStraightDistance(double tenthsOfIn, int masterPower)
   //Monitor 'totalTicks', instead of the values of the encoders which are constantly reset.
   while(std::abs(totalTicks) < tickGoal){
 
+
     //Proportional algorithm to keep the robot going straight.
     left_motor.SetPercent(masterPower);
     right_motor.SetPercent(slavePower);
 
-    error = left_encoder.Counts() - right_encoder.Counts();
+    error = (left_encoder.Counts()-leftPrior) - (right_encoder.Counts()-rightPrior);
+    leftPrior = left_encoder.Counts();
+    rightPrior = right_encoder.Counts();
+    slavePower += (error * kp) + (kd *(error - error_prior)) +(ki*error) ;
 
-    slavePower -= (error / kp) + (kd * (error - error_prior));
+   totalTicks = left_encoder.Counts();
+   writeScreen(left_encoder.Counts(), right_encoder.Counts(), masterPower, slavePower, totalTicks);
+   //left_encoder.ResetCounts();
+   //right_encoder.ResetCounts();
 
-    totalTicks+= left_encoder.Counts();
+   Sleep(2);
 
-    left_encoder.ResetCounts();
-    right_encoder.ResetCounts();
-
-    Sleep(5);
 
     error_prior= error;
 
@@ -58,12 +72,41 @@ void driveStraightDistance(double tenthsOfIn, int masterPower)
   }
   left_motor.Stop(); // Stop the loop once the encoders have counted up the correct number of encoder ticks.
   right_motor.Stop();
+
 }
+
 
 
 int main(void)
 {
-  //Distances specified in tenths of an inch.
+  LCD.Clear();  
+  RPS.InitializeTouchMenu();
+  int heading = RPS.Heading();
+  int x = RPS.X();
+  int y = RPS.Y();
 
-  driveStraightDistance(240,30);
+  LCD.SetBackgroundColor(WHITE);
+  LCD.SetFontColor(BLACK);
+
+  //Distances specified in tenths of an inch.
+  driveStraightDistance(180.80,30);
+  driveStraightDistance(97.77,60);
+  left_motor.Stop();
+  right_motor.Stop();
+  driveStraightDistance(12,20);
+  driveStraightDistance(178.51,30);
+  left_encoder.ResetCounts();
+  right_encoder.ResetCounts();
+  left_motor.SetPercent(40);
+  right_motor.SetPercent(17);
+  while(left_encoder.Counts()-right_encoder.Counts() < 415){}
+  left_motor.Stop();
+  right_motor.Stop();
+
+  LCD.WriteRC(heading, 1,0);//273
+  LCD.WriteRC(x,2,0);//5
+  LCD.WriteRC(y,3,0);//18
+
+
+
 }
