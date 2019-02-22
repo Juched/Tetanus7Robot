612e92695be30b11caf28dcdd947eea5aa1e1815
Evan <evanturner2018@gmail.com>
2019-02-21 20:58:09 -0500

    all new

--- main.cpp
+++ main.cpp
@@ -4,20 +4,33 @@
 #include <FEHMotor.h>
 #include <FEHRPS.h>
 
-AnalogInputPin cds(FEHIO::P0_0);
+AnalogInputPin cds(FEHIO::P3_2);
 FEHMotor rightMotor(FEHMotor::Motor0,9);
 FEHMotor leftMotor(FEHMotor::Motor1,9);
-AnalogEncoder rightEnc(FEHIO::P0_1);
-AnalogEncoder leftEnc(FEHIO::P0_2);
+DigitalEncoder rightEnc(FEHIO::P0_0);
+DigitalEncoder leftEnc(FEHIO::P0_7);
 
 float light = 1.5;
 int rightFaster = 1; //how many times faster right is than left
 float encInch = 40.5; //encodes/inch
-int turnSpeed = 10;
+int turnSpeed = 25;
 
 void resetCounts() {
     leftEnc.ResetCounts();
     rightEnc.ResetCounts();
+}
+
+void stop() {
+    leftMotor.Stop();
+    rightMotor.Stop();
+}
+
+void printEnc() {
+    LCD.Write("Right Encodes: ");
+    LCD.WriteLine(rightEnc.Counts());
+
+    LCD.Write("Left Encodes: ");
+    LCD.WriteLine(leftEnc.Counts());
 }
 
 float CDS() {
@@ -50,26 +63,51 @@ void driveStraightCounts(int counts, int power) {
     leftMotor.Stop();
 }
 
-void turnRight(int angle) {
+void driveStraightInches(int counts, int power) {
+    resetCounts();
+    int stop = counts*encInch;
+
+    leftMotor.SetPercent(power);
+    rightMotor.SetPercent(power*rightFaster);
+
+    while((leftEnc.Counts() < stop) || (rightEnc.Counts() < stop)) {
+        if(leftEnc.Counts() >= stop) {
+            leftMotor.Stop();
+        }
+        if(rightEnc.Counts() >= stop) {
+            rightMotor.Stop();
+        }
+    }
+
+    rightMotor.Stop();
+    leftMotor.Stop();
+}
+
+void turnRight(float angle) {
     float circ = 3.14159*2*3.5;
 
     float distance = (angle/360)*circ;
 
     resetCounts();
+
     leftMotor.SetPercent(turnSpeed);
-    while(leftEnc.Counts()<distance*encInch);
+    while(leftEnc.Counts()<(distance*encInch)) {
+        printEnc();
+    }
     leftMotor.Stop();
 
 }
 
-void turnLeft(int angle) {
+void turnLeft(float angle) {
     float circ = 3.14159*2*3.5;
 
     float distance = (angle/360)*circ;
 
     resetCounts();
     leftMotor.SetPercent(turnSpeed);
-    while(leftEnc.Counts()<distance*encInch);
+    while(leftEnc.Counts()<(distance*encInch)) {
+        printEnc();
+    }
     leftMotor.Stop();
 }
 
@@ -78,5 +116,8 @@ int main(void) {
     //wait for starting light
     CDS();
 
+    turnRight(45);
+    driveStraightCounts(800, -50);
+    driveStraightCounts(800, -50);
 
 }
